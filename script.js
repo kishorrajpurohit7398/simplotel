948724d1014a50490c916592de00f4527a655c40
Kishor Rajpurohit <130731045+kishorrajpurohit7398@users.noreply.github.com>
2023-05-03 21:23:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function openmenu() {
+    const nav = document.getElementById('nav');
+    nav.style.transform = "translateX(0px)";
+   
+}
+function closemenu() {
+  const nav = document.getElementById("nav");
+  nav.style.transform = "translateX(300px)";
+}
